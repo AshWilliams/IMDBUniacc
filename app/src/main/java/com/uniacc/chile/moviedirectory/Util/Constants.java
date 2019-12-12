bc880ac515bde9b753f159e9821bb7aa7d6d8063
Robert Rozas Navarro <rorozasn@microsoft.com>
2019-12-11 21:36:12 -0600

    trabajo uniacc, android imdb

---
+++ app/src/main/java/com/uniacc/chile/moviedirectory/Util/Constants.java
@@ -0,0 +1,12 @@
+package com.uniacc.chile.moviedirectory.Util;
+
+public class Constants {
+
+
+
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String API_KEY = "&apikey=ab71cbf0";
+    public static final String URL_DETAILS = "http://www.omdbapi.com/?i=";
+
+
+}
